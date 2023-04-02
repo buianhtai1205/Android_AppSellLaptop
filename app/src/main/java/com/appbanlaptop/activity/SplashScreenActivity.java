eead0c739d5f5a2c84e9a7f29b8af29bf8fd9eb0
buianhtai1205 <buianhtai2017tq@gmail.com>
2023-04-02 23:21:22 +0700

    add splash screen handle

--- app/src/main/java/com/appbanlaptop/activity/SplashScreenActivity.java
+++ app/src/main/java/com/appbanlaptop/activity/SplashScreenActivity.java
@@ -1,6 +1,8 @@
 package com.appbanlaptop.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,21 @@ import com.appbanlaptop.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
